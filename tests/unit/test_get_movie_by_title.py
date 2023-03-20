498b9e966fc5d3352fc8c5239b5e2f312ab02a88
EdwinMart2023 <123100388+EdwinMart2023@users.noreply.github.com>
2023-03-19 21:40:24 -0400

    Update test_get_movie_by_title.py

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,8 @@
 # TODO: Feature 3
+from app import app
+
+def test_get_movie_by_title():
+    with app.test_client() as client:
+        response = client.get('/movies/search?q=The%20Shawshank%20Redemption')
+        assert response.status_code == 200
+        assert b'The Shawshank Redemption' in response.data
