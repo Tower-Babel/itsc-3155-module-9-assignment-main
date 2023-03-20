28bc332398ba2aa6f935fc4f27d6bd267cb32d19
EdwinMart2023 <123100388+EdwinMart2023@users.noreply.github.com>
2023-03-19 21:38:33 -0400

    Update test_get_all_movies.py

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,8 @@
 # TODO: Feature 1
+from app import app
+
+def test_get_all_movies():
+    with app.test_client() as client:
+        response = client.get('/movies')
+        assert response.status_code == 200
+        assert b'All Movies' in response.data
